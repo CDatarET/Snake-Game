0a494fe031c2b389494196bce7fcb1c5f6c322e2
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-06-04 15:48:57 -0400

    Update snakegame.py

--- snakegame.py
+++ snakegame.py
@@ -3,11 +3,11 @@ import pygame
 print("hello world")  
 pygame.init()
 color = (0,0,0)
-rectColor = (0,255,0) 
+rectColor = (0,255,0)
 
 canvas = pygame.display.set_mode((500,500)) 
 
-pygame.display.set_caption("Snake") 
+pygame.display.set_caption("Snake")
 
 exit = False
   
@@ -17,5 +17,5 @@ while not exit:
         if event.type == pygame.QUIT: 
             exit = True
   
-    pygame.draw.rect(canvas, rectColor, pygame.Rect(250,250,20,20)) 
-    pygame.display.update() 
+    pygame.draw.rect(canvas, rectColor, pygame.Rect(250,250,20,20))
+    pygame.display.update()
